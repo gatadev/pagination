e1e788629fc3b2ccfd2fd3e5a514521166ea0047
gatadev <119637143+gatadev@users.noreply.github.com>
2022-12-27 12:49:08 -0500

    Update script.js

--- Pagination/js/script.js
+++ Pagination/js/script.js
@@ -14,21 +14,26 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 
 */
+let li = document.querySelector(".student-item");
+let header = document.getElementBtTagName(h3);
+let span = document.getElementBtTagName(span);
 
 function showPage() {
-  createElement();
-}
+createElement();
+};
 
-function createElement() {
-  const ul = document.getElementsByClassName(".student-list");
-  const div = document.getElementsByClassName(".student-details");
-  const img = document.createElement("img");
-  img.src = document.createElement("img").Value;
-  ul.appendChild(img);
-  ul.insertAfter(img, div);
-}
+const createElement = function() {
+   li.insertAdjacentHTML(
+     `<li> ${img.src}
+         <h3> ${header} </h3>
+         <span> ${span} </span>
+         <hr>
+ </li>`
+   );
+ }
 
 /*
+
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
