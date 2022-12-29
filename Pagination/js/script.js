35f8c6a49691918245a3373363000d4768c6197a
gatadev <119637143+gatadev@users.noreply.github.com>
2022-12-28 21:43:03 -0500

    Update script.js

--- Pagination/js/script.js
+++ Pagination/js/script.js
@@ -8,22 +8,24 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
+function showPage(list, page) {
+  // create two variables which will represent the index for the first and last student on the page
+  var itemPerPage = 9;
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+  var startIndex = page * itemPerPage - itemPerPage;
+  var endIndex = page * itemPerPage;
 
-*/
-let ul = document.querySelector(".student-list");
+  studentList.innerHTML = "";
 
-function showPage() {
-  for (let data = 0; data <= data.length; data++) {
-    return createElement[data];
-  }
-}
-
-const createElement = function(list, page) {
-  const li = `<li class="student-item cf">
+  // select the element with a class of `student-list` and assign it to a variable
+  var studentList = document.querySelector(".student-list");
+  // set the innerHTML property of the variable you just created to an empty string
+  studentList.innerHTML = "";
+  // loop over the length of the `list` parameter
+  list.forEach((list, index) => {
+    if (index >= startIndex && index <= endIndex) {
+      return student[index];
+      const studentItem = `<li class="student-item cf">
           <div class="student-details">
             <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
             <h3>Ethel Dean</h3>
@@ -33,11 +35,18 @@ const createElement = function(list, page) {
             <span class="date">Joined 12-15-2005</span>
           </div>
         </li>`;
-  ul.insertAdjacentHTML("beforeend", li);
-};
-showPage();
+      studentList.insertAdjacentHTML("beforeend", studentItem);
+    }
+  });
+}
+showPage(student, 1);
+//inside the loop create a conditional to display the proper students
+// inside the conditional:
+// create the elements needed to display the student information
+// insert the above elements
 
-/** 
+/*
+
 
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
