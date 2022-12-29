7d1b408b9dcc9bd34ef13999860a3fcf4fe58487
gatadev <119637143+gatadev@users.noreply.github.com>
2022-12-29 10:15:31 -0500

    Update script.js

--- Pagination/js/script.js
+++ Pagination/js/script.js
@@ -8,23 +8,32 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-function showPage(list, page) {
+
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+function showPage(data, page) {
   // create two variables which will represent the index for the first and last student on the page
   var itemPerPage = 9;
 
   var startIndex = page * itemPerPage - itemPerPage;
   var endIndex = page * itemPerPage;
 
-  studentList.innerHTML = "";
-
   // select the element with a class of `student-list` and assign it to a variable
   var studentList = document.querySelector(".student-list");
   // set the innerHTML property of the variable you just created to an empty string
   studentList.innerHTML = "";
   // loop over the length of the `list` parameter
-  list.forEach((list, index) => {
+
+  data.forEach((data, index) => {
     if (index >= startIndex && index <= endIndex) {
-      return student[index];
       const studentItem = `<li class="student-item cf">
           <div class="student-details">
             <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
@@ -39,7 +48,7 @@ function showPage(list, page) {
     }
   });
 }
-showPage(student, 1);
+showPage(data, 1);
 //inside the loop create a conditional to display the proper students
 // inside the conditional:
 // create the elements needed to display the student information
