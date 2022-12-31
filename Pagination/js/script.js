5c3b0e63ab8dd0a1190336811892e14eeefa5eb3
gatadev <119637143+gatadev@users.noreply.github.com>
2022-12-31 10:55:16 -0500

    Update script.js

--- Pagination/js/script.js
+++ Pagination/js/script.js
@@ -8,26 +8,16 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-function showPage(data, page) {
+var studentList = document.querySelector(".student-list");
+var itemPerPage = 9;
+!(function showPage(data, page) {
   // create two variables which will represent the index for the first and last student on the page
-  var itemPerPage = 9;
 
   var startIndex = page * itemPerPage - itemPerPage;
   var endIndex = page * itemPerPage;
 
   // select the element with a class of `student-list` and assign it to a variable
-  var studentList = document.querySelector(".student-list");
+
   // set the innerHTML property of the variable you just created to an empty string
   studentList.innerHTML = "";
   // loop over the length of the `list` parameter
@@ -36,19 +26,21 @@ function showPage(data, page) {
     if (index >= startIndex && index <= endIndex) {
       const studentItem = `<li class="student-item cf">
           <div class="student-details">
-            <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-            <h3>Ethel Dean</h3>
-            <span class="email">ethel.dean@example.com</span>
-          </div>
-          <div class="joined-details">
-            <span class="date">Joined 12-15-2005</span>
+          <img class="avatar" src= ${
+            data.picture.thumbnail
+          } alt="Profile Picture">
+          <h3>${data.name.first + " " + " " + data.name.last}</h3>
+          <span class="email">${data["email"]}/span>
+        </div>
+        <div class="joined-details">
+          <span class="date">Joined ${data.registered.date}</span>
           </div>
         </li>`;
       studentList.insertAdjacentHTML("beforeend", studentItem);
     }
   });
-}
-showPage(data, 1);
+})(data, 1);
+
 //inside the loop create a conditional to display the proper students
 // inside the conditional:
 // create the elements needed to display the student information
@@ -60,5 +52,38 @@ showPage(data, 1);
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+var linkList = document.querySelector(".link-list");
+!(function addPagination(list) {
+  // create a variable to calculate the number of pages needed
+  var numOfPages = Math.ceil(list.length / itemPerPage);
 
-// Call functions
+  // select the element with a class of `link-list` and assign it to a variable
+
+  // set the innerHTML property of the variable you just created to an empty string
+  linkList.innerHtml = "";
+  // loop over the number of pages needed
+  var i = 1;
+  for (var i = 1; i <= numOfPages.length; i++) {
+    var button = `<li>
+   
+   
+   
+
+   <button type="button">${numOfPages[i]}</button>
+   
+   linkList.insertAdjacentHTML('beforeend', button);
+  
+ </li>`;
+    // give the first pagination button a class of "active"
+    document.querySelector(li.button)[0].className = "active";
+    
+  }
+  // create the elements needed to display the pagination button
+  // insert the above elements
+
+  // create an event listener on the `link-list` element
+  // if the click target is a button:
+  // remove the "active" class from the previous button
+  // add the active class to the clicked button
+  // call the showPage function passing the `list` parameter and page to display as arguments
+})(data);
